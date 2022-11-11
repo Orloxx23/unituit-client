a8af817d47d1a6549c12932becb07edd1ac94326
Orlando Mina <65779635+Orloxx23@users.noreply.github.com>
2022-11-11 08:19:56 -0500

    proxy

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -4,7 +4,7 @@ export const loginCall = async (userCredential, dispatch) => {
   dispatch({ type: "LOGIN_START" });
   try {
     const res = await axios.post(
-      "https://unituit-api.herokuapp.com/api/auth/login",
+      "/auth/login",
       userCredential
     );
     dispatch({ type: "LOGIN_SUCCESS", payload: res.data });
